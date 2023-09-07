6cdf930abc0f40df6018adfd2b6aad7450dad7a9
HARSH SAHU <98934200+HarshSahu2001@users.noreply.github.com>
2023-09-07 22:31:10 +0530

    Add files via upload

---
+++ Day 5/Adding_Even_Number.py
@@ -0,0 +1,12 @@
+# Method 1
+sum = 0
+for number in range (1,101):
+    if number % 2 == 0:
+        sum += number
+print(sum)
+
+# #Method 2
+# Sum = 0
+# for number in range (0, 101, 2):
+#     Sum += number
+# print(Sum)
