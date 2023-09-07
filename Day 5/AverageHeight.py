8cd09eb92c499b2caff1d9eb8a35a02077be7175
HARSH SAHU <98934200+HarshSahu2001@users.noreply.github.com>
2023-09-07 14:52:31 +0530

    Add files via upload

---
+++ Day 5/AverageHeight.py
@@ -0,0 +1,18 @@
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+
+Height_Sum = 0
+Student_count = 0
+for height in student_heights:
+    Height_Sum += height
+    Student_count +=1
+
+Average = (Height_Sum/count)
+print(int(round(Average, 0)))
+
+# 2nd method
+# Total_Height = sum(student_heights)
+# students = len(student_heights)
+# Average = Total_Height/students
+# print (int(round(Average)))
