86e012f37ee2862c3742a458c19a2ae8cec22d94
HARSH SAHU <98934200+HarshSahu2001@users.noreply.github.com>
2023-09-10 14:47:47 +0530

    ReeborgWorld_Maze.py

--- Day 6/ReeborgWorld_Maze.py
+++ Day 6/ReeborgWorld_Maze.py
@@ -3,6 +3,9 @@ def turn_right():
     turn_left()
     turn_left()
 
+while front_is_clear():
+    move()
+turn_left()
 
 while at_goal() != True:
     if right_is_clear():
@@ -12,4 +15,3 @@ while at_goal() != True:
         move()
     else:
         turn_left()
-
